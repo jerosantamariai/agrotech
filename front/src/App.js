038f2515237617bd033781d3638d34cb96423ceb
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-08-05 23:10:03 -0400

    preparing a test to upload the last commit

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import injectContext from './store/appContext';
 import Home from './views/home';
 import NotFound from './views/notFound';
-import injectContext from './store/appContext';
+import Test from './views/test';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
+            <Route exact path="/test" component={Test} />
             <Route exact path="/" component={Home} />
             <Route component={NotFound} />
         </Switch>
